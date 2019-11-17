fd127010bbf81b64a88852ea437108221211679d
Scott Lee Davis <skawtus@gmail.com>
2019-11-16 20:42:00 -0800

    move flashcards location

--- dbt-mindset/src/App.vue
+++ dbt-mindset/src/App.vue
@@ -1,19 +1,33 @@
 <template>
   <div id="app">
     <h1>DBT Mindset</h1>
-    <div class="content">
-      <a class="twitter-timeline" data-width="800" data-height="550" data-theme="dark" data-link-color="#2B7BB9" href="https://twitter.com/dbtmindset?ref_src=twsrc%5Etfw">Tweets by dbtmindset</a>
-    </div>
     <div class="items">
       <ul>
         <li><a href="/flashcards/">DBT Flashcards</a></li>
       </ul>
+    </div>
+    <div class="content">
+      <a class="twitter-timeline" data-width="800" data-height="550" data-theme="dark" data-link-color="#2B7BB9" href="https://twitter.com/dbtmindset?ref_src=twsrc%5Etfw">Tweets by dbtmindset</a>
     </div>
   </div>
 </template>
 
 <script>
 
+export default {
+  name: 'app',
+  components: {
+  },
+  mounted() {
+  },
+  data() {
+    return {
+    }
+  },
+  methods: {
+  },
+
+}
 </script>
 
 <style>
